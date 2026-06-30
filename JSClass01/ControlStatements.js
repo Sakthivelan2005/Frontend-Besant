5085070d2cbf26c4486115201cb7dc768e56e8e7
Sakthivelan <sakthivelan.shankar@gmail.com>
2026-06-30 20:22:45 +0530

    learnings

---
+++ JSClass01/ControlStatements.js
@@ -0,0 +1,24 @@
+let a = +prompt("Enter a mark:")
+switch (true) {
+    case (a > 100):
+        a = prompt("Enter correct number within 100")
+        break;
+    case (a < 50):
+        console.log("Fail")
+        break;
+    case (a < 60):
+        console.log("Grade B")
+        break;
+    case (a < 75):
+        console.log("Grade A")
+        break;
+    case (a < 90):
+        console.log("Grade D")
+        break;
+    case (a <= 100):
+        console.log("Grade O")
+        break;
+    default:
+        console.log("Unexpected Error")
+        break;
+}
---
+++ JSClass01/VariablesAndOperators.js
@@ -0,0 +1,12 @@
+let a = Number(prompt("Enter a: ")), b = Number(prompt("Enter b: "))
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log(a++)
+console.log(++a)
+
+
+
